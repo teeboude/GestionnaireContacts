219d67775e17c89fe766c3cddc02c0124202ddfe
teeboude <marceldu03@gmail.com>
2023-02-09 12:41:57 +0100

    [Entity] Create relation table #24
    create relation table and manytomany + onetomany between contact and relation

---
+++ src/main/java/com/example/gestionnairecontacts/repository/entity/Relation.java
@@ -0,0 +1,45 @@
+package com.example.gestionnairecontacts.repository.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "relation_data")
+public class Relation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String lien;
+
+    @ManyToOne()
+    private Contact contactEntree;
+
+    @ManyToOne()
+    private Contact contactSortie;
+
+    public Relation() {
+    }
+
+    // getters et setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLien() {
+        return lien;
+    }
+
+    public void setLien(String lien) {
+        this.lien = lien;
+    }
+
+
+
+}
